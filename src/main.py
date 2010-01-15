3e9d8979c0b94510a38e4c743cd0ea72df6075d0
Arthur Wang <arthraim@gmail.com>
2010-01-15 10:45:44 +0800

    Fixed some problem caused by encoding. index.html only show first 10 recent users.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 # coding=UTF8
-
 import cgi
 import os
 
@@ -20,11 +19,11 @@ class Renjianer(db.Model):
     right_id = db.IntegerProperty()
 
 
-class MainHandler(webapp.RequestHandler):
+class RootHandler(webapp.RequestHandler):
 
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        rens = db.GqlQuery("SELECT * FROM Renjianer ORDER BY date DESC")
+        rens = db.GqlQuery("SELECT * FROM Renjianer ORDER BY date DESC LIMIT 10")
         template_values = {"rens": rens}
         path = os.path.join(os.path.dirname(__file__), 'template/index.html')
         self.response.out.write(template.render(path, template_values))
@@ -32,40 +31,40 @@ class MainHandler(webapp.RequestHandler):
 
 class Neighbourhood(webapp.RequestHandler):
     def post(self):
-        screen_name = cgi.escape(self.request.get('content'))
+        screen_name = self.request.get('content')
         
         rnjn_url = 'http://api.renjian.com/'
         # 1、请求user信息
-        user_url = rnjn_url + 'users/show.json?id=' + screen_name;
-        result = urlfetch.fetch(user_url)  
+        user_url = rnjn_url + 'users/show.json?id=' + unicode(screen_name);
+        result = urlfetch.fetch(user_url)
         if result.status_code == 200:
             json = result.content
             you = simplejson.loads(str(json))
-            ren = Renjianer(key_name=you['screen_name'])
-            ren.user_name = you['screen_name']
+            ren = Renjianer(key_name=unicode(you['screen_name']))
+            ren.user_name = unicode(you['screen_name'])
             ren.user_id = you['id']
         else:
-            you = {'error': str(result.status_code)}
+            you = {'error': unicode(result.status_code)}
         # 2、请求左边用户信息
-        user_url = rnjn_url + 'users/show.json?id=' + str(ren.user_id - 1);
-        result = urlfetch.fetch(user_url)  
+        user_url = rnjn_url + 'users/show.json?id=' + unicode(ren.user_id - 1);
+        result = urlfetch.fetch(user_url)
         if result.status_code == 200:
             json = result.content
-            left = simplejson.loads(str(json))
-            ren.left_name = left['screen_name']
+            left = simplejson.loads(unicode(json))
+            ren.left_name = unicode(left['screen_name'])
             ren.left_id = left['id']
         else:
-            you = {'error': str(result.status_code)}
+            you = {'error': unicode(result.status_code)}
         # 3、请求右边用户信息
-        user_url = rnjn_url + 'users/show.json?id=' + str(ren.user_id + 1);
-        result = urlfetch.fetch(user_url)  
+        user_url = rnjn_url + 'users/show.json?id=' + unicode(ren.user_id + 1);
+        result = urlfetch.fetch(user_url)
         if result.status_code == 200:
             json = result.content
-            right = simplejson.loads(str(json))
-            ren.right_name = right['screen_name']
+            right = simplejson.loads(unicode(json))
+            ren.right_name = unicode(right['screen_name'])
             ren.right_id = right['id']
         else:
-            you = {'error': str(result.status_code)}
+            you = {'error': unicode(result.status_code)}
         
         ren.put()
         
@@ -82,7 +81,7 @@ class Neighbourhood(webapp.RequestHandler):
 
 def main():
     application = webapp.WSGIApplication([
-                                          ('/', MainHandler),
+                                          ('/', RootHandler),
                                           ('/neighbourhood', Neighbourhood)
                                          ],
                                          debug=True)
