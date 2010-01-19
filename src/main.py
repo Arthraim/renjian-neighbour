c26402d34f1f4b355b03b82a6fdbe4ae0f1444bf
Arthur Wang <arthraim@gmail.com>
2010-01-19 10:52:59 +0800

    Added some code to handle the exeptions. Handle the problem when API got error. Fixed @niedhui doesn't have a left neighbour. Also the last user may doesn't have a right neighbour.

--- src/main.py
+++ src/main.py
@@ -44,45 +44,56 @@ class Neighbourhood(webapp.RequestHandler):
                 ren.user_name = you['screen_name']
                 ren.user_id = you['id']
             else:
-                you = {'error': result.status_code}
-            # 2、请求左边用户信息
-            user_url = rnjn_url + u'users/show.json?id=' + unicode(ren.user_id - 1);
-            result = urlfetch.fetch(user_url)
-            if result.status_code == 200:
-                json = result.content
-                left = simplejson.loads(json, encoding='UTF-8')
-                ren.left_name = left['screen_name']
-                ren.left_id = left['id']
-            else:
-                you = {'error': result.status_code}
-            # 3、请求右边用户信息
-            user_url = rnjn_url + u'users/show.json?id=' + unicode(ren.user_id + 1);
-            result = urlfetch.fetch(user_url)
-            if result.status_code == 200:
-                json = result.content
-                right = simplejson.loads(json, encoding='UTF-8')
-                ren.right_name = right['screen_name']
-                ren.right_id = right['id']
-            else:
-                you = {'error': result.status_code}
+                you = {'error': 'urlfetch result: ' + unicode(result.status_code)}
+                left = {'error': 'Fetching your infomation fail.' }
+                right = {'error': 'Fetching your infomation fail.' }
             
-            ren.put()
-            
-        except Exception,ex:
-            self.redirect('/error?message=' + 'Exception: ' + unicode(ex));
-            return
-        
-        template_values = {
-            'you': you,
-            'left': left,
-            'right': right,
-            }
-        
-        path = os.path.join(os.path.dirname(__file__), 'template/result.html')
-        self.response.out.write(template.render(path, template_values))
-        #self.redirect('/')
+            if not you.has_key('error'):
+                # 2、请求左边用户信息
+                user_url = rnjn_url + u'users/show.json?id=' + unicode(ren.user_id - 1);
+                result = urlfetch.fetch(user_url)
+                if result.status_code == 200:
+                    json = result.content
+                    left = simplejson.loads(json, encoding='UTF-8')
+                    ren.left_name = left['screen_name']
+                    ren.left_id = left['id']
+                else:
+                    left = {'error': 'urlfetch result: ' + unicode(result.status_code)}
+                # 3、请求右边用户信息
+                user_url = rnjn_url + u'users/show.json?id=' + unicode(ren.user_id + 1);
+                result = urlfetch.fetch(user_url)
+                if result.status_code == 200:
+                    json = result.content
+                    right = simplejson.loads(json, encoding='UTF-8')
+                    ren.right_name = right['screen_name']
+                    ren.right_id = right['id']
+                else:
+                    right = {'error': 'urlfetch result: ' + unicode(result.status_code)}
+                # 4、放入数据库
+                ren.put()
 
-        
+            if you.has_key('error'):
+                you['message'] = "Fetching your infomation fail!<br /> Did you spell your <strong>screen_name</strong> or <strong>id</strong> correctly?"
+            if left.has_key('error'):
+                left['message'] = "Finding your left neighbour may got a problem, or you don't even have one :)."
+            if right.has_key('error'):
+                right['message'] = "Finding your left neighbour may got a problem, or you don't even have one :)."
+
+            template_values = {
+                'you': you,
+                'left': left,
+                'right': right,
+                }
+            
+            path = os.path.join(os.path.dirname(__file__), 'template/result.html')
+            self.response.out.write(template.render(path, template_values))
+            #self.redirect('/')
+
+        except Exception,ex:
+            self.redirect(u'/error?message=' + 'Exception: ' + unicode(ex));
+            return
+
+
 class UsersHandler(webapp.RequestHandler):
     def get(self):
         rens = db.GqlQuery("SELECT * FROM Renjianer ORDER BY date DESC")
